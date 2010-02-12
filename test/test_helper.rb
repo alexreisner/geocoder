b3fb9addde24e13420317c0f97eaa20bf7ad916f
alexreisner <alex@alexreisner.com>
2010-02-11 23:04:30 -0500

    Require 'active_support/core_ext' instead of 'activesupport'.
    This makes the tests Rails 3 compatible. Adding /core_ext is necessary
    or we don't have access to the class_inheritable_reader method.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'test/unit'
-require 'activesupport'
+require 'active_support/core_ext'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
