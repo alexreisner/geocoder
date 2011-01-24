9736ab57b848b0b4806fbca37c4cabf542fa4e2c
Alex Reisner <alex@alexreisner.com>
2011-01-24 16:14:57 -0500

    Remove unneeded curly braces.

--- lib/geocoder.rb
+++ lib/geocoder.rb
@@ -11,22 +11,22 @@ ActiveRecord::Base.class_eval do
   # Set attribute names and include the Geocoder module.
   #
   def self.geocoded_by(address_attr, options = {})
-    _geocoder_init({
+    _geocoder_init(
       :address   => address_attr,
       :latitude  => options[:latitude]  || :latitude,
       :longitude => options[:longitude] || :longitude
-    })
+    )
   end
 
   ##
   # Set attribute names and include the Geocoder module.
   #
   def self.reverse_geocoded_by(latitude_attr, longitude_attr, options = {})
-    _geocoder_init({
+    _geocoder_init(
       :address   => options[:address] || :address,
       :latitude  => latitude_attr,
       :longitude => longitude_attr
-    })
+    )
   end
 
   def self._geocoder_init(options)
