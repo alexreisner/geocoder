f4a4ec75cea43653f2ac44c6cee2441cd052e424
Alex Reisner <alex@alexreisner.com>
2011-03-04 10:23:28 -0500

    Kill dependency on cattr_accessor.

--- lib/geocoder/configuration.rb
+++ lib/geocoder/configuration.rb
@@ -1,6 +1,13 @@
 module Geocoder
   class Configuration
-    cattr_accessor :timeout, :lookup, :yahoo_appid
+    def self.timeout; @@timeout; end
+    def self.timeout=(obj); @@timeout = obj; end
+
+    def self.lookup; @@lookup; end
+    def self.lookup=(obj); @@lookup = obj; end
+
+    def self.yahoo_appid; @@yahoo_appid; end
+    def self.yahoo_appid=(obj); @@yahoo_appid = obj; end
   end
 end
 
