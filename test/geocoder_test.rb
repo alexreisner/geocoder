f0ec431901d6fdcd2ea69a9dcbc3c4e21cfe0b27
Alex Reisner <alex@alexreisner.com>
2011-03-09 00:17:41 -0500

    Add tests for fetch_x: should be alias.

--- test/geocoder_test.rb
+++ test/geocoder_test.rb
@@ -94,6 +94,20 @@ class GeocoderTest < Test::Unit::TestCase
     assert_not_nil g.location
   end
 
+  def test_fetch_coordinates_is_alias_for_geocode
+    v = Venue.new(*venue_params(:msg))
+    coords = [40.750354, -73.993371]
+    assert_equal coords, v.fetch_coordinates
+    assert_equal coords, [v.latitude, v.longitude]
+  end
+
+  def test_fetch_address_is_alias_for_reverse_geocode
+    v = Landmark.new(*landmark_params(:msg))
+    address = "4 Penn Plaza, New York, NY 10001, USA"
+    assert_equal address, v.fetch_address
+    assert_equal address, v.address
+  end
+
 
   # --- Google ---
 
