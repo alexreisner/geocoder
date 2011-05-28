b16060ed144c75efc28a738860d975155bb2d5f2
Adnan Ali <adnan.ali@gmail.com>
2011-05-28 19:26:10 -0400

    adding mongoid test for customer coordinate field in near scope.

---
+++ test/mongoid_test_helper.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+Mongoid.configure do |config| 
+  config.logger = Logger.new($stderr, :debug)
+end 
+
+##
+# Geocoded model.
+#
+class Place
+	include Mongoid::Document
+	include Geocoder::Model::Mongoid
+
+  geocoded_by :address, :coordinates => :location
+	field :name
+	field :address
+	field :location, :type => Array
+
+  def initialize(name, address)
+    super()
+    write_attribute :name, name
+    write_attribute :address, address
+  end
+end
