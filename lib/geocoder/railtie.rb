6d4a1c67aee2cccf5c3dae2719f2946c64d04158
Peter McCracken <peter@petermccracken.com>
2011-10-26 16:22:22 -0400

    Aborting Railtie initialization when RAILS_GROUPS is 'assets' and config.assets.initialize_on_precompile is false.
    This prevents an error on rake assets:precompile to a regular machine,
    and also avoids an error on heroku (because initialize_on_precompile
    must be false for Heroku).
    
    I'm still not convinced this is the correct way to do this (seems silly
    for geocoder to know about asset precompilation) but I'm not otherwise
    sure how initializers can be configured.

--- lib/geocoder/railtie.rb
+++ lib/geocoder/railtie.rb
@@ -5,9 +5,11 @@ module Geocoder
   if defined? Rails::Railtie
     require 'rails'
     class Railtie < Rails::Railtie
-      initializer 'geocoder.insert_into_active_record' do
-        ActiveSupport.on_load :active_record do
-          Geocoder::Railtie.insert
+      initializer 'geocoder.insert_into_active_record' do |app|
+        unless ENV['RAILS_GROUPS'].to_s == 'assets' && !app.config.assets.initialize_on_precompile
+          ActiveSupport.on_load :active_record do
+            Geocoder::Railtie.insert
+          end
         end
       end
       rake_tasks do
