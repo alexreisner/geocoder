78a660e14d10e187c15f9f1dd538ee07424a2374
Brett Hammond <bretthammond@stubthumb.com>
2012-05-29 18:48:19 -0600

    Updated

--- lib/geocoder/models/mongoid.rb
+++ lib/geocoder/models/mongoid.rb
@@ -20,7 +20,7 @@ module Geocoder
           # index [[ geocoder_options[:coordinates], Mongo::GEO2D ]],
           #             :min => -180, :max => 180 # create 2d index
             
-          index geocoder_options[:coordinates] "2d" #Updated to work with new Mongoid 3.0.0rc
+          index geocoder_options[:coordinates] => "2d" #Updated to work with new Mongoid 3.0.0rc
         end
       end
     end
