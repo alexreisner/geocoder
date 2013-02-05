e35930dcd661c6936c4f1c84de1d4907f598289c
Idris <sld7700@gmail.com>
2013-02-05 22:52:29 +0600

    Added test to yandex no city and no town case

--- test/result_test.rb
+++ test/result_test.rb
@@ -4,18 +4,36 @@ require 'test_helper'
 class ResultTest < Test::Unit::TestCase
 
   def test_result_has_required_attributes
-    all_lookups.each do |l|
-      Geocoder::Configuration.lookup = l
+    Geocoder::Lookup.all_services_except_test.each do |l|
+      Geocoder.configure(:lookup => l)
+      set_api_key!(l)
       result = Geocoder.search([45.423733, -75.676333]).first
       assert_result_has_required_attributes(result)
     end
   end
 
 
+  # Redefine Yandex to work with fixture without city and town
+  class Yandex
+    private
+    def default_fixture_filename
+      "yandex_no_city_and_town"
+    end
+  end
+
+
+  def test_yandex_result_without_city_has_not_raises_exception
+    Geocoder.configure(:lookup => :yandex)
+    set_api_key!(:yandex)
+    result = Geocoder.search([45.423733, -75.676333]).first
+    assert result.city, ""
+  end
+
+
   private # ------------------------------------------------------------------
 
   def assert_result_has_required_attributes(result)
-    m = "Lookup #{Geocoder::Configuration.lookup} does not support %s attribute."
+    m = "Lookup #{Geocoder.config.lookup} does not support %s attribute."
     assert result.coordinates.is_a?(Array),    m % "coordinates"
     assert result.latitude.is_a?(Float),       m % "latitude"
     assert result.longitude.is_a?(Float),      m % "longitude"
