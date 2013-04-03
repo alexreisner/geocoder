45353b00338a89d37d151cdca98b6308e421c796
ian asaff <ian.asaff@gmail.com>
2013-04-03 18:06:57 -0400

    add support for setting a default stub in test mode. updated readme to reflect addition.

--- lib/geocoder/lookups/test.rb
+++ lib/geocoder/lookups/test.rb
@@ -13,8 +13,13 @@ module Geocoder
         stubs[query_text] = results
       end
 
+      def self.set_default_stub(results)
+        @default_stub = results
+      end
+
       def self.read_stub(query_text)
         stubs.fetch(query_text) {
+          return @default_stub unless @default_stub.nil?
           raise ArgumentError, "unknown stub request #{query_text}"
         }
       end
@@ -25,6 +30,7 @@ module Geocoder
 
       def self.reset
         @stubs = {}
+        @default_stub = nil
       end
 
       private
