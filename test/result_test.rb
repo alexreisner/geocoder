7e887e8d9cdc157537e9c17bb5b28e3e6acd1819
Alexey Elizarov <felex-ae@ya.ru>
2013-12-17 17:18:30 +0300

    Update result_test.rb

--- test/result_test.rb
+++ test/result_test.rb
@@ -26,7 +26,7 @@ class ResultTest < Test::Unit::TestCase
       Geocoder.configure(:lookup => :yandex)
       set_api_key!(:yandex)
       result = Geocoder.search("new york").first
-      assert_equal "New York", result.city
+      assert_equal "", result.city
     end
   end
 
