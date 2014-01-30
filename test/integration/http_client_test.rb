a25e3d7b8ef3dba9954a624b2441b7e018b5f111
Alex Reisner <alex@alexreisner.com>
2014-01-29 21:35:21 -0500

    Add more useful/specific integration tests.

---
+++ test/integration/http_client_test.rb
@@ -0,0 +1,24 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), *%w[ .. .. lib]))
+require 'pathname'
+require 'rubygems'
+require 'test/unit'
+require 'geocoder'
+require 'yaml'
+
+class HttpClientTest < Test::Unit::TestCase
+  def setup
+    @api_keys = YAML.load_file("api_keys.yml")
+  end
+
+  def test_http_basic_auth
+    Geocoder.configure(lookup: :geocoder_us, api_key: @api_keys["geocoder_us"])
+    results = Geocoder.search "27701"
+    assert_not_nil (r = results.first)
+  end
+
+  def test_ssl
+    Geocoder.configure(lookup: :esri, use_https: true)
+    results = Geocoder.search "27701"
+    assert_not_nil (r = results.first)
+  end
+end
