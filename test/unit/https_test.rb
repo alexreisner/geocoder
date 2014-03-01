6646ecea2c210d4042c86d4147b225a5c68a51a9
Alex Reisner <alex@alexreisner.com>
2014-03-01 18:09:55 -0500

    Fix Ruby warnings.

--- test/unit/https_test.rb
+++ test/unit/https_test.rb
@@ -7,11 +7,11 @@ class HttpsTest < GeocoderTestCase
   def test_uses_https_for_secure_query
     Geocoder.configure(:use_https => true)
     g = Geocoder::Lookup::Google.new
-    assert_match /^https:/, g.query_url(Geocoder::Query.new("test"))
+    assert_match(/^https:/, g.query_url(Geocoder::Query.new("test")))
   end
 
   def test_uses_http_by_default
     g = Geocoder::Lookup::Google.new
-    assert_match /^http:/, g.query_url(Geocoder::Query.new("test"))
+    assert_match(/^http:/, g.query_url(Geocoder::Query.new("test")))
   end
 end
