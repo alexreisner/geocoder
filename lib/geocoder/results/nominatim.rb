4a2bda9b601c0bedb3f753ac07a59a25fba931d1
Torsten Wetzel <torsten@orderlift.com>
2015-02-26 11:52:11 +0100

    add more results to google and nominatim

--- lib/geocoder/results/nominatim.rb
+++ lib/geocoder/results/nominatim.rb
@@ -78,6 +78,56 @@ module Geocoder::Result
       @data['type']
     end
 
+
+    ## Additional Stuff
+    
+    def city
+      %w[city town village hamlet].each do |key|
+        return @data['address'][key] if @data['address'].key?(key)
+      end
+      @data['display_name'].split(',').count > 3 ? @data['display_name'].split(',')[-3].strip : nil
+      # return nil
+    end
+    
+    def street
+      %w[road pedestrian highway].each do |key|
+        return @data['address'][key] if @data['address'].key?(key)
+      end
+      return nil
+    end
+    
+    def street_number
+      @data['address']['house_number']
+    end
+    
+    def country_code
+      @data['address']['country_code'].present? ? @data['address']['country_code'].to_s.upcase : nil
+    end
+    
+    def district
+      @data['address']['city_district'].present? ? @data['address']['city_district'] : nil
+    end
+    
+    def subdistrict
+      @data['address']['suburb'].present? ? @data['address']['suburb'] : nil
+    end
+    
+    def city_code
+      %w[city town village hamlet].each do |key|
+        return @data['address'][key] if @data['address'].key?(key)
+      end
+      @data['display_name'].split(',').count > 3 ? @data['display_name'].split(',')[-3].strip : nil
+    end
+    
+    def district_code
+      @data['address']['city_district'].present? ? @data['address']['city_district'] : nil
+    end
+    
+    def subdistrict_code
+      @data['address']['suburb'].present? ? @data['address']['suburb'] : nil
+    end
+
+
     def self.response_attributes
       %w[place_id osm_type osm_id boundingbox license
          polygonpoints display_name class type stadium]
