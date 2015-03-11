fd09201e3ef792c2c146a2e2ee6c443b8055e1cd
Thu Trang Pham <thuutrangpham@gmail.com>
2015-03-11 15:54:52 -0400

    Fixing NearTest

--- test/unit/near_test.rb
+++ test/unit/near_test.rb
@@ -6,7 +6,8 @@ class NearTest < GeocoderTestCase
 
   def test_near_scope_options_without_sqlite_includes_bounding_box_condition
     result = PlaceWithCustomResultsHandling.send(:near_scope_options, 1.0, 2.0, 5)
-    assert_match(/test_table_name.latitude BETWEEN 0.9276\d* AND 1.0723\d* AND test_table_name.longitude BETWEEN 1.9276\d* AND 2.0723\d* AND /, result[:conditions][0])
+    table_name = PlaceWithCustomResultsHandling.table_name
+    assert_match(/#{table_name}.latitude BETWEEN 0.9276\d* AND 1.0723\d* AND #{table_name}.longitude BETWEEN 1.9276\d* AND 2.0723\d* AND /, result[:conditions][0])
   end
 
   def test_near_scope_options_without_sqlite_includes_radius_condition
