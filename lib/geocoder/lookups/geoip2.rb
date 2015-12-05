12517ddc3f4cf3bb25b9488abacad17f59b4dca6
Omachonu Ogali <oogali@idlepattern.com>
2015-12-05 07:07:22 -0500

    Instantiate objects in constructor, rather than for each request

--- lib/geocoder/lookups/geoip2.rb
+++ lib/geocoder/lookups/geoip2.rb
@@ -12,6 +12,12 @@ module Geocoder
           rescue LoadError
             raise "Could not load Maxmind DB dependency. To use the GeoIP2 lookup you must add the #{@gem_name} gem to your Gemfile or have it installed in your system."
           end
+
+          if @gem_name == 'hive_geoip2'
+            @mmdb = Hive::GeoIP2.new(configuration[:file].to_s)
+          else
+            @mmdb = MaxMindDB.new(configuration[:file].to_s)
+          end
         end
         super
       end
@@ -28,11 +34,8 @@ module Geocoder
 
       def results(query)
         return [] unless configuration[:file]
-        if @gem_name == 'hive_geoip2'
-          result = Hive::GeoIP2.lookup(query.to_s, configuration[:file].to_s)
-        else
-          result = MaxMindDB.new(configuration[:file].to_s).lookup(query.to_s)
-        end
+
+        result = @mmdb.lookup(query.to_s)
         result.nil? ? [] : [result]
       end
     end
