c9588580b79108d1f4c9bf5c467018e2ff62e235
Juan C. Müller <jcmuller@gmail.com>
2016-11-15 13:06:22 -0500

    Extract context method

--- lib/geocoder/results/mapbox.rb
+++ lib/geocoder/results/mapbox.rb
@@ -16,31 +16,37 @@ module Geocoder::Result
     end
 
     def city
-      @data['context'].map { |c| c['text'] if c['id'] =~ /place/ }.compact.first
+      context.map { |c| c['text'] if c['id'] =~ /place/ }.compact.first
     end
 
     def state
-      @data['context'].map { |c| c['text'] if c['id'] =~ /region/ }.compact.first
+      context.map { |c| c['text'] if c['id'] =~ /region/ }.compact.first
     end
 
     alias_method :state_code, :state
 
     def postal_code
-      @data['context'].map { |c| c['text'] if c['id'] =~ /postcode/ }.compact.first
+      context.map { |c| c['text'] if c['id'] =~ /postcode/ }.compact.first
     end
 
     def country
-      @data['context'].map { |c| c['text'] if c['id'] =~ /country/ }.compact.first
+      context.map { |c| c['text'] if c['id'] =~ /country/ }.compact.first
     end
 
     alias_method :country_code, :country
 
     def neighborhood
-      @data['context'].map { |c| c['text'] if c['id'] =~ /neighborhood/ }.compact.first
+      context.map { |c| c['text'] if c['id'] =~ /neighborhood/ }.compact.first
     end
 
     def address
       [place_name, street, city, state, postal_code, country].compact.join(", ")
+    end
+
+    private
+
+    def context
+      @data['context']
     end
   end
 end
