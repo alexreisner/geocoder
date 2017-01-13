e6b8b8dcbf2f7e82e605971c8fe0ee1879645d90
Taufiq Muhammadi <waruboy@yahoo.com>
2017-01-13 18:13:13 +0700

    implement blank method

--- lib/geocoder/results/google_places_search.rb
+++ lib/geocoder/results/google_places_search.rb
@@ -15,6 +15,38 @@ module Geocoder
       def photos
         @data["photos"]
       end
+
+      def city
+        ""
+      end
+
+      def state
+        ""
+      end
+
+      def state_code
+        ""
+      end
+
+      def province
+        ""
+      end
+
+      def province_code
+        ""
+      end
+
+      def postal_code
+        ""
+      end
+
+      def country
+        ""
+      end
+
+      def country_code
+        ""
+      end
     end
   end
 end
