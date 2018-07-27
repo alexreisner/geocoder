266e9543d604d0dccb8f525d94f4e57e3fb65ab5
ip2location <support@ip2location.com>
2018-07-27 10:42:45 +0800

    Update Result class and test.

--- lib/geocoder/results/ip2location_api.rb
+++ lib/geocoder/results/ip2location_api.rb
@@ -8,93 +8,83 @@ module Geocoder::Result
     end
 
     def country_code
-      country_code
+      @data['country_code']
     end
 
     def country_name
-      country_name
+      @data['country_name']
     end
 
     def region_name
-      region_name
+      @data['region_name']
     end
 
     def city_name
-      city_name
+      @data['city_name']
     end
 
     def latitude
-      latitude
+      @data['latitude']
     end
 
     def longitude
-      longitude
+      @data['longitude']
     end
 
     def zip_code
-      zip_code
+      @data['zip_code']
     end
 
     def time_zone
-      time_zone
+      @data['time_zone']
     end
 
     def isp
-      isp
+      @data['isp']
     end
 
     def domain
-      domain
+      @data['domain']
     end
 
     def net_speed
-      net_speed
+      @data['net_speed']
     end
 
     def idd_code
-      idd_code
+      @data['idd_code']
     end
 
     def area_code
-      area_code
+      @data['area_code']
     end
 
     def weather_station_code
-      weather_station_code
+      @data['weather_station_code']
     end
 
     def weather_station_name
-      weather_station_name
+      @data['weather_station_name']
     end
 
     def mcc
-      mcc
+      @data['mcc']
     end
 
     def mnc
-      mnc
+      @data['mnc']
     end
 
     def mobile_brand
-      mobile_brand
+      @data['mobile_brand']
     end
 
     def elevation
-      elevation
+      @data['elevation']
     end
 
     def usage_type
-      usage_type
-    end
-
-    def self.response_attributes
-      %w[country_code country_name region_name city_name latitude longitude zip_code time_zone isp domain net_speed idd_code area_code weather_station_code weather_station_name mcc mnc mobile_brand elevation usage_type]
-    end
-
-    response_attributes.each do |attribute|
-      define_method attribute do
-        @data[attribute]
-      end
+      @data['usage_type']
     end
 
   end
