e506cd6db932fcd9cd1ae1ac344bbf801e4de0a2
Zach Whitehead <zachmanw@gmail.com>
2018-09-10 10:40:36 -0400

    Add GooglePlacesDetailsLimited lookup

---
+++ lib/geocoder/lookups/google_places_details_limited.rb
@@ -0,0 +1,51 @@
+require "geocoder/lookups/google"
+require "geocoder/results/google_places_details_limited"
+
+module Geocoder
+  module Lookup
+    class GooglePlacesDetailsLimited < Google
+      def name
+        "Google Places Details"
+      end
+
+      def required_api_key_parts
+        ["key"]
+      end
+
+      def supported_protocols
+        [:https]
+      end
+
+      private
+
+      def base_query_url(query)
+        "#{protocol}://maps.googleapis.com/maps/api/place/details/json?"
+      end
+
+      def results(query)
+        return [] unless doc = fetch_data(query)
+
+        case doc["status"]
+        when "OK"
+          return [doc["result"]]
+        when "OVER_QUERY_LIMIT"
+          raise_error(Geocoder::OverQueryLimitError) || Geocoder.log(:warn, "Google Places Details API error: over query limit.")
+        when "REQUEST_DENIED"
+          raise_error(Geocoder::RequestDenied) || Geocoder.log(:warn, "Google Places Details API error: request denied.")
+        when "INVALID_REQUEST"
+          raise_error(Geocoder::InvalidRequest) || Geocoder.log(:warn, "Google Places Details API error: invalid request.")
+        end
+
+        []
+      end
+
+      def query_url_google_params(query)
+        {
+          placeid: query.text,
+          language: query.language || configuration.language,
+          fields: ["name", "geometry.location", "place_id", "formatted_address"]
+        }
+      end
+    end
+  end
+end
