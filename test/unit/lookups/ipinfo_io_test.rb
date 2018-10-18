d1e03c81e50066196e75328ce8434cd9d18a79bb
Ryan Dlugosz <ryan@dlugosz.net>
2018-10-18 11:12:03 -0400

    Allow https for IPInfo.io queries without an API key
    It appears that at some point they changed the "TLS only for paying
    customers" rule.
    
    See: https://ipinfo.io/developers#https-ssl
    "Our API is available over a secure HTTPS connection for all users, even
    on the free plan."

--- test/unit/lookups/ipinfo_io_test.rb
+++ test/unit/lookups/ipinfo_io_test.rb
@@ -2,19 +2,6 @@
 require 'test_helper'
 
 class IpinfoIoTest < GeocoderTestCase
-
-  def test_ipinfo_io_use_http_without_token
-    Geocoder.configure(:ip_lookup => :ipinfo_io, :use_https => true)
-    query = Geocoder::Query.new("8.8.8.8")
-    assert_match(/^http:/, query.url)
-  end
-
-  def test_ipinfo_io_uses_https_when_auth_token_set
-    Geocoder.configure(:ip_lookup => :ipinfo_io, :api_key => "FOO_BAR_TOKEN", :use_https => true)
-    query = Geocoder::Query.new("8.8.8.8")
-    assert_match(/^https:/, query.url)
-  end
-
   def test_ipinfo_io_lookup_loopback_address
     Geocoder.configure(:ip_lookup => :ipinfo_io)
     result = Geocoder.search("127.0.0.1").first
