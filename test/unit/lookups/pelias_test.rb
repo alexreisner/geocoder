5e01e8acd203f82481c0a56e6f40aea01a06cc60
Jorge Santos <jorgeoliveirasantos@gmail.com>
2018-10-19 10:54:01 +0800

    Modify Pelias lookup to return multiple results
    - Fixes #1353
      - Do not limit the results returned from Pelias to 1;
      - Removed test that was enforcing this condition;

--- test/unit/lookups/pelias_test.rb
+++ test/unit/lookups/pelias_test.rb
@@ -17,14 +17,9 @@ class PeliasTest < GeocoderTestCase
     assert_true query.url.start_with?('http://self.hosted.pelias/proxy/v1/search'), query.url
   end
 
-  def test_query_url_defaults_to_one
-    query = Geocoder::Query.new('Madison Square Garden, New York, NY')
-    assert_match 'size=1', query.url
-  end
-
   def test_query_for_reverse_geocode
     lookup = Geocoder::Lookup::Pelias.new
     url = lookup.query_url(Geocoder::Query.new([45.423733, -75.676333]))
-    assert_match(/point.lat=45.423733&point.lon=-75.676333&size=1/, url)
+    assert_match(/point.lat=45.423733&point.lon=-75.676333/, url)
   end
 end
