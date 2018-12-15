9a00cdcb5f6735d0a9dba445408eaf9ce0214bfc
Andrew Perkins <aperkins81@gmail.com>
2018-12-15 16:27:29 +1000

    Fix: compass_point, 341-348 degrees missing N value in 16-wind compass rose

--- lib/geocoder/calculations.rb
+++ lib/geocoder/calculations.rb
@@ -155,7 +155,7 @@ module Geocoder
     #
     def compass_point(bearing, points = COMPASS_POINTS)
       seg_size = 360 / points.size
-      points[((bearing + (seg_size / 2)) % 360) / seg_size]
+      points[(((bearing + (seg_size / 2)) % 360) / seg_size) % points.size]
     end
 
     ##
