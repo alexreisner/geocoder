4a356edae46735138690a4cae762ca6475f3089f
David Kelly <david@futuresbright.com>
2019-03-07 14:33:38 +0100

    A few extra assertions in test

--- test/unit/lookups/nationaal_georegister_nl_test.rb
+++ test/unit/lookups/nationaal_georegister_nl_test.rb
@@ -8,12 +8,16 @@ class NationaalGeoregisterNlTest < GeocoderTestCase
   end
 
   def test_result_components
-    result = Geocoder.search('Nieuwezijds Voorburgwal 147, 1012 RJ Amsterdam').first
+    result = Geocoder.search('Nieuwezijds Voorburgwal 147, Amsterdam').first
 
-    assert_equal result.city,         'Amsterdam'
-    assert_equal result.postal_code,  '1012RJ'
-    assert_equal result.address,      'Nieuwezijds Voorburgwal 147, 1012RJ Amsterdam'
-    assert_equal result.country_code, 'NL'
+    assert_equal result.street,         'Nieuwezijds Voorburgwal'
+    assert_equal result.street_number,  '147'
+    assert_equal result.city,           'Amsterdam'
+    assert_equal result.postal_code,    '1012RJ'
+    assert_equal result.address,        'Nieuwezijds Voorburgwal 147, 1012RJ Amsterdam'
+    assert_equal result.province,       'Noord-Holland'
+    assert_equal result.province_code,  'PV27'
+    assert_equal result.country_code,   'NL'
   end
 
 end
