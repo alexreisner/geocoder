d43bd610bb653a8e33001f99279188b1ce2b0acb
David Kelly <david@futuresbright.com>
2019-03-07 14:54:26 +0100

    Move queries params out of base_query_url()

--- lib/geocoder/lookups/nationaal_georegister_nl.rb
+++ lib/geocoder/lookups/nationaal_georegister_nl.rb
@@ -11,11 +11,11 @@ module Geocoder::Lookup
     private # ---------------------------------------------------------------
 
     def base_query_url(query)
-      "#{protocol}://geodata.nationaalgeoregister.nl/locatieserver/v3/free?fl=*&q="
+      "#{protocol}://geodata.nationaalgeoregister.nl/locatieserver/v3/free?"
     end
 
     def valid_response?(response)
-      json = parse_json(response.body)
+      json   = parse_json(response.body)
       status = json["status"] if json
       super(response) and ['OK', 'ZERO_RESULTS'].include?(status)
     end
@@ -25,35 +25,11 @@ module Geocoder::Lookup
       return doc['response']['docs']
     end
 
-    def query_url_google_params(query)
-      params = {
-        :sensor => "false",
-        :language => (query.language || configuration.language)
-      }
-      if query.options[:google_place_id]
-        params[:place_id] = query.sanitized_text
-      else
-        params[(query.reverse_geocode? ? :latlng : :address)] = query.sanitized_text
-      end
-      unless (bounds = query.options[:bounds]).nil?
-        params[:bounds] = bounds.map{ |point| "%f,%f" % point }.join('|')
-      end
-      unless (region = query.options[:region]).nil?
-        params[:region] = region
-      end
-      unless (components = query.options[:components]).nil?
-        params[:components] = components.is_a?(Array) ? components.join("|") : components
-      end
-      unless (result_type = query.options[:result_type]).nil?
-        params[:result_type] = result_type.is_a?(Array) ? result_type.join("|") : result_type
-      end
-      params
-    end
-
     def query_url_params(query)
-      query_url_google_params(query).merge(
-        :key => configuration.api_key
-      ).merge(super)
+      {
+        fl: '*',
+        q:  query.text
+      }.merge(super)
     end
   end
 end
