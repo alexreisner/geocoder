ecb2aefa97442838bf7440d40bb945d94ae7a82b
David Kelly <david@futuresbright.com>
2019-04-25 14:03:41 +0200

    Bugfix - valid_response?

--- lib/geocoder/lookups/nationaal_georegister_nl.rb
+++ lib/geocoder/lookups/nationaal_georegister_nl.rb
@@ -10,14 +10,17 @@ module Geocoder::Lookup
 
     private # ---------------------------------------------------------------
 
+    def cache_key(query)
+      base_query_url(query) + hash_to_query(query_url_params(query))
+    end
+
     def base_query_url(query)
       "#{protocol}://geodata.nationaalgeoregister.nl/locatieserver/v3/free?"
     end
 
     def valid_response?(response)
       json   = parse_json(response.body)
-      status = json["status"] if json
-      super(response) and ['OK', 'ZERO_RESULTS'].include?(status)
+      super(response) if json
     end
 
     def results(query)
