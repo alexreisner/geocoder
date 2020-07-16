31dfaa6358b46541085fe3465ad70c445d201569
Maximilien Tyc <maximilien.tyc@gmail.com>
2020-07-16 14:21:43 +0200

    Google place search (#1)

--- lib/geocoder/lookups/google_places_search.rb
+++ lib/geocoder/lookups/google_places_search.rb
@@ -18,15 +18,41 @@ module Geocoder
 
       private
 
+      def results(query)
+        super(query, 'candidates')
+      end
+
       def base_query_url(query)
-        "#{protocol}://maps.googleapis.com/maps/api/place/textsearch/json?"
+        "#{protocol}://maps.googleapis.com/maps/api/place/findplacefromtext/json?"
       end
 
       def query_url_google_params(query)
         {
-          query: query.text,
+          input: query.text,
+          inputtype: 'textquery',
+          fields: fields(query),
           language: query.language || configuration.language
         }
+      end
+
+      def fields(query)
+        query_fields = query.options[:fields]
+        return format_fields(query_fields) if query_fields
+
+        default_fields
+      end
+
+      def default_fields
+        legacy = %w[id reference]
+        basic = %w[business_status formatted_address geometry icon name 
+          photos place_id plus_code types]
+        contact = %w[opening_hours]
+        atmosphere = %W[price_level rating user_ratings_total]
+        format_fields(legacy, basic, contact, atmosphere)
+      end
+
+      def format_fields(*fields)
+        fields.flatten.join(',')
       end
     end
   end
--- test/unit/lookups/google_places_search_test.rb
+++ test/unit/lookups/google_places_search_test.rb
@@ -33,9 +33,39 @@ class GooglePlacesSearchTest < GeocoderTestCase
     assert_match(/language=de/, url)
   end
 
+  def test_google_places_search_query_url_contains_input
+    url = lookup.query_url(Geocoder::Query.new("some-address"))
+    assert_match(/input=some-address/, url)
+  end
+
+  def test_google_places_search_query_url_contains_input_typer
+    url = lookup.query_url(Geocoder::Query.new("some-address"))
+    assert_match(/inputtype=textquery/, url)
+  end
+
   def test_google_places_search_query_url_always_uses_https
     url = lookup.query_url(Geocoder::Query.new("some-address"))
-    assert_match(%r(^https://), url)
+    assert_match(%r{^https://}, url)
+  end
+
+  def test_google_places_search_query_url_contains_every_field_available_by_default
+    url = lookup.query_url(Geocoder::Query.new("some-address"))
+    fields = %w[id reference business_status formatted_address geometry icon name 
+      photos place_id plus_code types opening_hours price_level rating 
+      user_ratings_total]
+    assert_match(/fields=#{fields.join('%2C')}/, url)
+  end
+
+  def test_google_places_search_query_url_contains_specific_fields_when_given
+    fields = %w[formatted_address place_id]
+    url = lookup.query_url(Geocoder::Query.new("some-address", fields: fields))
+    
+    assert_match(/fields=#{fields.join('%2C')}/, url)
+  end
+
+  def test_google_places_search_query_url_uses_find_place_service
+    url = lookup.query_url(Geocoder::Query.new("some-address"))
+    assert_match(%r{//maps.googleapis.com/maps/api/place/findplacefromtext/}, url)
   end
 
   private
