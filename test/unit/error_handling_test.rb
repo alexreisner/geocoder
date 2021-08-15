2f83e149999a82c00a4c6fcc7f0ccfb596046b4d
Matt Lewis <matt@mplewis.com>
2021-08-14 18:32:37 -0600

    fix: exclude AWS ALS from HTTP response tests

--- test/unit/error_handling_test.rb
+++ test/unit/error_handling_test.rb
@@ -9,7 +9,7 @@ class ErrorHandlingTest < GeocoderTestCase
 
   def test_does_not_choke_on_timeout
     silence_warnings do
-      Geocoder::Lookup.all_services_except_test.each do |l|
+      Geocoder::Lookup.all_services_with_http_requests.each do |l|
         Geocoder.configure(:lookup => l)
         set_api_key!(l)
         assert_nothing_raised { Geocoder.search("timeout") }
@@ -42,7 +42,7 @@ class ErrorHandlingTest < GeocoderTestCase
 
   def test_always_raise_timeout_error
     Geocoder.configure(:always_raise => [Timeout::Error])
-    Geocoder::Lookup.all_services_except_test.each do |l|
+    Geocoder::Lookup.all_services_with_http_requests.each do |l|
       next if l == :maxmind_local || l == :geoip2 # local, does not use cache
       lookup = Geocoder::Lookup.get(l)
       set_api_key!(l)
@@ -54,7 +54,7 @@ class ErrorHandlingTest < GeocoderTestCase
 
   def test_always_raise_socket_error
     Geocoder.configure(:always_raise => [SocketError])
-    Geocoder::Lookup.all_services_except_test.each do |l|
+    Geocoder::Lookup.all_services_with_http_requests.each do |l|
       next if l == :maxmind_local || l == :geoip2 # local, does not use cache
       lookup = Geocoder::Lookup.get(l)
       set_api_key!(l)
@@ -66,7 +66,7 @@ class ErrorHandlingTest < GeocoderTestCase
 
   def test_always_raise_connection_refused_error
     Geocoder.configure(:always_raise => [Errno::ECONNREFUSED])
-    Geocoder::Lookup.all_services_except_test.each do |l|
+    Geocoder::Lookup.all_services_with_http_requests.each do |l|
       next if l == :maxmind_local || l == :geoip2 # local, does not use cache
       lookup = Geocoder::Lookup.get(l)
       set_api_key!(l)
@@ -78,7 +78,7 @@ class ErrorHandlingTest < GeocoderTestCase
 
   def test_always_raise_host_unreachable_error
     Geocoder.configure(:always_raise => [Errno::EHOSTUNREACH])
-    Geocoder::Lookup.all_services_except_test.each do |l|
+    Geocoder::Lookup.all_services_with_http_requests.each do |l|
       next if l == :maxmind_local || l == :geoip2 # local, does not use cache
       lookup = Geocoder::Lookup.get(l)
       set_api_key!(l)
