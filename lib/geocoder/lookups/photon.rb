42d8aa5fd8881c97a141a96f1a0d2cd2d2f9f9f8
Alex Reisner <alex@alexreisner.com>
2021-08-31 20:35:55 -0600

    Update code formatting (for consistency).

--- lib/geocoder/lookups/photon.rb
+++ lib/geocoder/lookups/photon.rb
@@ -1,90 +1,85 @@
-# frozen_string_literal: true
-
 require 'geocoder/lookups/base'
 require 'geocoder/results/photon'
 
-module Geocoder
-  module Lookup
-    # https://github.com/komoot/photon
-    class Photon < Base
-      def name
-        'Photon'
+module Geocoder::Lookup
+  class Photon < Base
+    def name
+      'Photon'
+    end
+
+    private # ---------------------------------------------------------------
+
+    def base_query_url(query)
+      host = configuration[:host] || 'photon.komoot.io'
+      method = query.reverse_geocode? ? 'reverse' : 'api'
+      "#{protocol}://#{host}/#{method}?"
+    end
+
+    def results(query)
+      return [] unless (doc = fetch_data(query))
+      return [] unless doc['type'] == 'FeatureCollection'
+      return [] unless doc['features'] || doc['features'].present?
+
+      doc['features']
+    end
+
+    def query_url_params(query)
+      lang = query.language || configuration.language
+      params = { lang: lang, limit: query.options[:limit] }
+
+      if query.reverse_geocode?
+        params.merge!(query_url_params_reverse(query))
+      else
+        params.merge!(query_url_params_coordinates(query))
       end
 
-      private
+      params.merge!(super)
+    end
 
-      def base_query_url(query)
-        host = configuration[:host] || 'photon.komoot.io'
-        method = query.reverse_geocode? ? 'reverse' : 'api'
-        "#{protocol}://#{host}/#{method}?"
+    def query_url_params_coordinates(query)
+      params = { q: query.sanitized_text }
+
+      if (bias = query.options[:bias])
+        params.merge!(lat: bias[:latitude], lon: bias[:longitude], location_bias_scale: bias[:scale])
       end
 
-      def results(query)
-        return [] unless (doc = fetch_data(query))
-        return [] unless doc['type'] == 'FeatureCollection'
-        return [] unless doc['features'] || doc['features'].present?
-
-        doc['features']
+      if (filter = query_url_params_coordinates_filter(query))
+        params.merge!(filter)
       end
 
-      def query_url_params(query)
-        lang = query.language || configuration.language
-        params = { lang: lang, limit: query.options[:limit] }
+      params
+    end
 
-        if query.reverse_geocode?
-          params.merge!(query_url_params_reverse(query))
-        else
-          params.merge!(query_url_params_coordinates(query))
-        end
+    def query_url_params_coordinates_filter(query)
+      filter = query.options[:filter]
+      return unless filter
 
-        params.merge!(super)
+      bbox = filter[:bbox]
+      {
+        bbox: bbox.is_a?(Array) ? bbox.join(',') : bbox,
+        osm_tag: filter[:osm_tag]
+      }
+    end
+
+    def query_url_params_reverse(query)
+      params = { lat: query.coordinates[0], lon: query.coordinates[1], radius: query.options[:radius] }
+
+      if (dsort = query.options[:distance_sort])
+        params[:distance_sort] = dsort ? 'true' : 'false'
       end
 
-      def query_url_params_coordinates(query)
-        params = { q: query.sanitized_text }
-
-        if (bias = query.options[:bias])
-          params.merge!(lat: bias[:latitude], lon: bias[:longitude], location_bias_scale: bias[:scale])
-        end
-
-        if (filter = query_url_params_coordinates_filter(query))
-          params.merge!(filter)
-        end
-
-        params
+      if (filter = query_url_params_reverse_filter(query))
+        params.merge!(filter)
       end
 
-      def query_url_params_coordinates_filter(query)
-        filter = query.options[:filter]
-        return unless filter
+      params
+    end
 
-        bbox = filter[:bbox]
-        {
-          bbox: bbox.is_a?(Array) ? bbox.join(',') : bbox,
-          osm_tag: filter[:osm_tag]
-        }
-      end
+    def query_url_params_reverse_filter(query)
+      filter = query.options[:filter]
+      return unless filter
 
-      def query_url_params_reverse(query)
-        params = { lat: query.coordinates[0], lon: query.coordinates[1], radius: query.options[:radius] }
-
-        if (dsort = query.options[:distance_sort])
-          params[:distance_sort] = dsort ? 'true' : 'false'
-        end
-
-        if (filter = query_url_params_reverse_filter(query))
-          params.merge!(filter)
-        end
-
-        params
-      end
-
-      def query_url_params_reverse_filter(query)
-        filter = query.options[:filter]
-        return unless filter
-
-        { query_string_filter: filter[:string] }
-      end
+      { query_string_filter: filter[:string] }
     end
   end
 end
--- lib/geocoder/results/photon.rb
+++ lib/geocoder/results/photon.rb
@@ -1,122 +1,117 @@
-# frozen_string_literal: true
-
 require 'geocoder/results/base'
 
-module Geocoder
-  module Result
-    # https://github.com/komoot/photon
-    class Photon < Base
-      def name
-        properties['name']
-      end
+module Geocoder::Result
+  class Photon < Base
+    def name
+      properties['name']
+    end
 
-      def address(_format = :full)
-        parts = []
-        parts << name if name
-        parts << street_address if street_address
-        parts << city
-        parts << state if state
-        parts << postal_code
-        parts << country
+    def address(_format = :full)
+      parts = []
+      parts << name if name
+      parts << street_address if street_address
+      parts << city
+      parts << state if state
+      parts << postal_code
+      parts << country
 
-        parts.join(', ')
-      end
+      parts.join(', ')
+    end
 
-      def street_address
-        return unless street
-        return street unless house_number
+    def street_address
+      return unless street
+      return street unless house_number
 
-        "#{house_number} #{street}"
-      end
+      "#{house_number} #{street}"
+    end
 
-      def house_number
-        properties['housenumber']
-      end
+    def house_number
+      properties['housenumber']
+    end
 
-      def street
-        properties['street']
-      end
+    def street
+      properties['street']
+    end
 
-      def postal_code
-        properties['postcode']
-      end
+    def postal_code
+      properties['postcode']
+    end
 
-      def city
-        properties['city']
-      end
+    def city
+      properties['city']
+    end
 
-      def state
-        properties['state']
-      end
+    def state
+      properties['state']
+    end
 
-      def state_code
-        ''
-      end
+    def state_code
+      ''
+    end
 
-      def country
-        properties['country']
-      end
+    def country
+      properties['country']
+    end
 
-      def country_code
-        ''
-      end
+    def country_code
+      ''
+    end
 
-      def coordinates
-        return unless geometry
-        return unless geometry[:coordinates]
+    def coordinates
+      return unless geometry
+      return unless geometry[:coordinates]
 
-        geometry[:coordinates].reverse
-      end
+      geometry[:coordinates].reverse
+    end
 
-      def geometry
-        return unless data['geometry']
+    def geometry
+      return unless data['geometry']
 
-        symbol_hash data['geometry']
-      end
+      symbol_hash data['geometry']
+    end
 
-      def bounds
-        properties['extent']
-      end
+    def bounds
+      properties['extent']
+    end
 
-      # Type of the result (OSM object type), one of:
-      #
-      #   :node
-      #   :way
-      #   :relation
-      #
-      def type
-        {
-          'N' => :node,
-          'W' => :way,
-          'R' => :relation
-        }[properties['osm_type']]
-      end
+    # Type of the result (OSM object type), one of:
+    #
+    #   :node
+    #   :way
+    #   :relation
+    #
+    def type
+      {
+        'N' => :node,
+        'W' => :way,
+        'R' => :relation
+      }[properties['osm_type']]
+    end
 
-      def osm_id
-        properties['osm_id']
-      end
+    def osm_id
+      properties['osm_id']
+    end
 
-      # See: https://wiki.openstreetmap.org/wiki/Tags
-      def osm_tag
-        return unless properties['osm_key']
-        return properties['osm_key'] unless properties['osm_value']
+    # See: https://wiki.openstreetmap.org/wiki/Tags
+    def osm_tag
+      return unless properties['osm_key']
+      return properties['osm_key'] unless properties['osm_value']
 
-        "#{properties['osm_key']}=#{properties['osm_value']}"
-      end
+      "#{properties['osm_key']}=#{properties['osm_value']}"
+    end
 
-      private
+    private
 
-      def properties
-        @properties ||= data['properties'] || {}
-      end
+    def properties
+      @properties ||= data['properties'] || {}
+    end
 
-      def symbol_hash(orig_hash)
-        {}.tap do |result|
-          orig_hash.each_key do |key|
-            next unless orig_hash[key]
+    def symbol_hash(orig_hash)
+      {}.tap do |result|
+        orig_hash.each_key do |key|
+          next unless orig_hash[key]
 
-            result[key.to_sym] = orig_hash[key]
-          end
+          result[key.to_sym] = orig_hash[key]
         end
       end
     end
