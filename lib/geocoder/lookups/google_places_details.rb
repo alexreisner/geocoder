ac5a618ec69ca0ce1351bf24f570848199d9880e
Chuan-Zheng Lee <chuan-zheng@kamiapp.com>
2022-04-07 08:55:01 +1200

    Add ability to set fields in configuration for Google Places

--- lib/geocoder/lookups/google_places_details.rb
+++ lib/geocoder/lookups/google_places_details.rb
@@ -33,9 +33,24 @@ module Geocoder
         result
       end
 
+      def fields(query)
+        query_fields = query.options[:fields]
+        return format_fields(query_fields) if query_fields
+
+        config_fields = configuration[:fields]
+        return format_fields(config_fields) if config_fields
+
+        nil  # use Google Places defaults
+      end
+
+      def format_fields(*fields)
+        fields.flatten.join(',')
+      end
+
       def query_url_google_params(query)
         {
           placeid: query.text,
+          fields: fields(query),
           language: query.language || configuration.language
         }
       end
