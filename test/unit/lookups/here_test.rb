e6d9ef10115213c4f3e63679639bbed9d38c0b5a
Alex Reisner <alex@alexreisner.com>
2022-05-17 10:09:37 -0700

    Remove unnecessary test.

--- test/unit/lookups/here_test.rb
+++ test/unit/lookups/here_test.rb
@@ -14,11 +14,6 @@ class HereTest < GeocoderTestCase
     assert_raises(Geocoder::ConfigurationError) { Geocoder.search("Berlin").first }
   end
 
-  def test_with_array_api_key_no_result_when_not_raised
-    Geocoder.configure(api_key: %w[foo bar])
-    assert_equal Geocoder.search("Berlin"), []
-  end
-
   def test_here_viewport
     result = Geocoder.search("Berlin").first
     assert_equal [52.33812, 13.08835, 52.6755, 13.761],
