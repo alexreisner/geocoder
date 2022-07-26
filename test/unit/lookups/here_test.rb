5c663668b0760a3987ca8fb4935cc184a31c44f3
Alex Reisner <alex@alexreisner.com>
2022-07-25 19:25:17 -0600

    Print result for inspection.

--- test/unit/lookups/here_test.rb
+++ test/unit/lookups/here_test.rb
@@ -17,6 +17,7 @@ class HereTest < GeocoderTestCase
 
   def test_here_viewport
     result = Geocoder.search("Berlin").first
+    puts "RESULT: #{result.data}"
     assert_equal [52.33812, 13.08835, 52.6755, 13.761], result.viewport
   end
 
