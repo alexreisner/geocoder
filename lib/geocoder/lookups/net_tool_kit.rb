b95624e74a5de71dcfcb7ae258c79ba75e3d4df0
Jeff Barriault <jeff@barriault.net>
2022-10-18 10:11:43 -0500

    add version=2 to base query url

--- lib/geocoder/lookups/net_tool_kit.rb
+++ lib/geocoder/lookups/net_tool_kit.rb
@@ -1,11 +1,11 @@
 require 'geocoder/lookups/base'
-require "geocoder/results/net_tool_kit"
+require 'geocoder/results/net_tool_kit'
 
 module Geocoder::Lookup
   class NetToolKit < Base
 
     def name
-      "NetToolKit"
+      'NetToolKit'
     end
 
     def supported_protocols
@@ -15,34 +15,34 @@ module Geocoder::Lookup
     private # ---------------------------------------------------------------
 
     def base_query_url(query)
-      "#{protocol}://api.nettoolkit.com/v1/geo/#{search_type(query)}?"
+      "#{protocol}://api.nettoolkit.com/v1/geo/#{search_type(query)}?version=2&"
     end
 
     def search_type(query)
-      query.reverse_geocode? ? "reverse-geocodes" : "geocodes"
+      query.reverse_geocode? ? 'reverse-geocodes' : 'geocodes'
     end
 
     def query_url_params(query)
-      params = { :address => query.sanitized_text }
-      
+      params = { address: query.sanitized_text }
+
       [:provider].each do |option|
         unless (option_value = query.options[option]).nil?
           params[option] = option_value
         end
       end
-      
+
       params.merge(super)
     end
 
     def results(query)
       return [] unless doc = fetch_data(query)
-      return doc["results"] if doc['code'] == 1000 || doc['code'] == 1001
-      
+      return doc['results'] if doc['code'] == 1000 || doc['code'] == 1001
+
       messages = doc['message']
       case doc['code']
       when 2002, 2003, 2004, 2005
         raise_error(Geocoder::ServiceUnavailable, messages) ||
-            Geocoder.log(:warn, "NetToolKit Geocoding API error: #{messages}")
+          Geocoder.log(:warn, "NetToolKit Geocoding API error: #{messages}")
       when 3000
         raise_error(Geocoder::RequestDenied, messages) ||
           Geocoder.log(:warn, "NetToolKit Geocoding API error: #{messages}")
