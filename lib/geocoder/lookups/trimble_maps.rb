ee3c66773d78afa42e91980c0f369665bbfd9379
Alex Dean <alexdean@transfix.io>
2023-05-13 16:24:39 -0500

    don't depend on activesupport

--- lib/geocoder/lookups/trimble_maps.rb
+++ lib/geocoder/lookups/trimble_maps.rb
@@ -24,7 +24,7 @@ module Geocoder::Lookup
 
     def base_query_url(query)
       region_code = region(query)
-      if !region_code.in?(valid_region_codes)
+      if !valid_region_codes.include?(region_code)
         raise "region_code '#{region_code}' is invalid. use one of #{valid_region_codes}." \
           "https://developer.trimblemaps.com/restful-apis/location/single-search/single-search-api/#test-the-api-now"
       end
