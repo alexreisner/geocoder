5d47085e87501c54e6a1f134833342b70470803f
Michael Agra <agramichael@gmail.com>
2025-01-28 15:37:59 +0000

    Add support for Ordnance Survey Places API

---
+++ lib/geocoder/results/uk_ordnance_survey_places.rb
@@ -0,0 +1,55 @@
+require 'geocoder/results/base'
+require 'easting_northing'
+
+module Geocoder::Result
+  class UkOrdnanceSurveyPlaces < Base
+
+    def coordinates
+      @coordinates ||= Geocoder::EastingNorthing.new(
+        easting: data['X_COORDINATE'],
+        northing: data['Y_COORDINATE'],
+      ).lat_lng
+    end
+
+    def city
+      data['POST_TOWN']
+    end
+
+    def county
+      data['DEPENDENT_LOCALITY']
+    end
+    alias state county
+
+    def county_code
+      ''
+    end
+    alias state_code county_code
+
+    def province
+      {
+        'E' => 'England',
+        'W' => 'Wales',
+        'S' => 'Scotland',
+        'N' => 'Northern Ireland',
+        'L' => 'Channel Islands',
+        'M' => 'Isle of Man',
+      }[data['COUNTRY_CODE']]
+    end
+
+    def province_code
+      data['COUNTRY_CODE']
+    end
+
+    def postal_code
+      data['POSTCODE']
+    end
+
+    def country
+      'United Kingdom'
+    end
+
+    def country_code
+      'UK'
+    end
+  end
+end
